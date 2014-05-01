74bbcb2b2d61f31f58c10c5876d6925839a9ac9f
saikiranmothe <saikiran.mothe@gmail.com>
2014-05-01 18:35:09 +0530

    added while loop

---
+++ php/while.php
@@ -0,0 +1,17 @@
+<h1 style="text-align:center"> While Loop in  PHP </h1>
+<?php 
+for ($x=0; $x<=10; $x++) {
+  echo "The number is: $x <br>";
+} 
+echo("<hr>");
+/*
+	for (init counter; test counter; increment counter) {
+  code to be executed;
+}
+*/
+/*  Special Loop For Harish */
+for ($a = 19 ; $a <= 23 ; $a++) { 
+	echo("Harish GIRL FRIENDS :GIRLNAME$a.<br>");
+}
+
+?>
