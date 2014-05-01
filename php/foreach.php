a105ddcc4f32356bd169b1856a70c0739d0e6bf6
saikiranmothe <saikiran.mothe@gmail.com>
2014-05-01 18:42:02 +0530

    updated with foreach loop

---
+++ php/foreach.php
@@ -0,0 +1,30 @@
+<?php 
+/*
+	foreach ($array as $value) {
+	  code to be executed;
+	}
+*/
+
+#example form w3schools
+$colors = array("red","green","blue","yellow"); 
+foreach ($colors as $value) {
+  echo "$value <br>";
+}
+/*
+
+foreach ($variable as $key => $value) {
+	# code...
+}*
+
+*/
+
+#Array 
+$a = array("Tina","meena","veena","Sona");
+foreach ($a as  $gfname) {
+	echo("<hr>");
+	echo("Harish Girl Friends : <h1>$gfname </h1>");
+}
+
+
+
+?>
