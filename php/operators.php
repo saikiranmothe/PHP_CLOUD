f82a1cbd0fccf0727d77d242e951546abb220900
saikiranmothe <saikiran.mothe@gmail.com>
2014-05-01 17:42:08 +0530

    updated operators in php

--- php/operators.php
+++ php/operators.php
@@ -29,3 +29,79 @@ $y=6;
  echo($hr);
  echo($a % $b); # 0
 ?>
+
+<h3> Assignment Operators</h3>
+<?php 
+#PHP Assignment Operators
+//Assignment operators
+/* 
+
+	x = y	x = y	The left operand gets set to the value of the expression on the right
+	x += y	x = x + y	Addition
+	x -= y	x = x - y	Subtraction
+	x *= y	x = x * y	Multiplication
+	x /= y	x = x / y	Division
+	x %= y	x = x % y	Modulus
+*/
+
+	$x = 12;
+	$y = 11;
+	/*  
+		x = y	x = y	The left operand gets set to the value of the expression on the right
+		x += y	x = x + y	Addition
+		x -= y	x = x - y	Subtraction
+		x *= y	x = x * y	Multiplication
+		x /= y	x = x / y	Division
+		x %= y	x = x % y	Modulus
+
+	*/
+	echo $x = $y;	//x = y
+	echo $hr;  
+	echo $x++;
+	echo($hr); 
+	echo(+$x); // Post INCREMENT
+	echo($hr);
+	echo($x += $y); // x + y 
+	echo($hr);
+	echo($x -= $y);  // x - y
+	echo($hr);
+	echo($x %= y)
+
+?>
+
+<!-- Example :P -->
+<?php 
+
+$x=10; 
+echo $x; // outputs 10
+echo($hr);
+
+$y=20; 
+$y += 100;
+echo $y; // outputs 120
+echo($hr);
+
+$z=50;
+$z -= 25;
+echo $z; // outputs 25
+echo($hr);
+
+
+$i=5;
+$i *= 6;
+echo $i; // outputs 30
+echo($hr);
+
+
+$j=10;
+$j /= 5;
+echo $j; // outputs 2
+echo($hr);
+
+
+$k=15;
+$k %= 4;
+echo $k; // outputs 3
+echo($hr);
+
+?>
